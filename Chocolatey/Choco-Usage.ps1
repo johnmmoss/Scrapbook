3e4e9d9cd394f8bdd6d57706f2b066f11d63eb8f
johnmmoss <johnmmoss@gmail.com>
2017-04-26 09:11:19 +0100

    Create Choco-Usage.ps1

---
+++ Chocolatey/Choco-Usage.ps1
@@ -0,0 +1,14 @@
+## Help
+choco -?
+
+## More help
+choco install -help
+
+## List locally installed packages
+choco list -l
+
+## List remote repository packages
+choco list
+
+## Choco install without prompt
+choco install notepadplusplus -y
