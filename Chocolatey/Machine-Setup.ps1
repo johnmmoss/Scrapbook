08aaa8e866938c7c4c0b670cd7b7d3586e8f6d29
johnmmoss <johnmmoss@gmail.com>
2017-04-26 09:08:36 +0100

    Create Machine-Setup.ps1

---
+++ Chocolatey/Machine-Setup.ps1
@@ -0,0 +1,17 @@
+
+choco install 7zip -y
+choco install notepadplusplus -y
+choco install winmerge -y
+choco install VirtualCloneDrive -y
+choco install GoogleChrome -y
+choco install vim -y
+choco install sysinternals -y
+choco install treesizefree -y
+
+choco install git -y
+choco install gitkraken -y
+
+choco install VisualStudio2013Premium -y
+choco install VisualStudio2015Professional -y
+choco install VisualStudio2017Professional -y #  Needs further install see jpg
+choco install VisualStudioCode -y
