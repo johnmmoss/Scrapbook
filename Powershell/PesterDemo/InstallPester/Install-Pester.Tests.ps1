e9be639e7142e399d76a9fc319515fc8cbbc8a76
John Moss <john.moss@zenith.co.uk>
2017-04-28 13:56:39 +0100

    Added a Pester test for the Setup-Pester cmdlet

---
+++ Powershell/PesterDemo/InstallPester/Install-Pester.Tests.ps1
@@ -0,0 +1,76 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Install-Pester" {
+    #
+    # GLOBAL MOCKS
+    #
+    Mock Write-Host        { }    # Override Write-Host so that messages arent printed during test run
+    Mock Invoke-Expression { }
+    Mock Get-Module        { }
+    Mock Import-Module     { }
+    #
+    # Test Directories
+    #
+    mkdir TestDrive:\Modules 
+    mkdir TestDrive:\Modules\Pester
+   
+    Context "First removes existing Pester install "{
+        It "by deleting Pester folder" {
+            
+            (Test-Path TestDrive:\Modules\Pester) | Should be $true
+
+            Install-Pester -ModulePath TestDrive:\Modules  
+            
+            (Test-Path TestDrive:\Modules\Pester) | Should be $false
+        }
+    }
+
+    Context "Then clones from git " {
+        
+        Mock Get-Location {  }
+
+        It "first changes dir to modules dir " {
+
+            Mock Set-Location {  }   -ParameterFilter { $Path -eq 'TestDrive:\Modules' }
+
+            Install-Pester -ModulePath TestDrive:\Modules  
+
+            Assert-MockCalled Set-Location 
+        }
+
+        It "then calls git.exe and clones to modules dir " {
+
+            Install-Pester -ModulePath TestDrive:\Modules  
+
+            Assert-MockCalled Invoke-Expression
+        }
+
+        It "finishes by reverting the dir back " {
+
+            Mock Set-Location {  }   -ParameterFilter { $Path -eq $here }
+
+            Install-Pester -ModulePath TestDrive:\Modules  
+
+            Assert-MockCalled Set-Location
+        }
+    }
+
+    Context "Finishes by" {
+
+        It "importing the module" {
+            
+            Install-Pester -ModulePath TestDrive:\Modules  
+
+            Assert-MockCalled Import-Module
+        }
+
+        It "then lists the available commands" {
+
+            Install-Pester -ModulePath TestDrive:\Modules  
+
+            Assert-MockCalled Get-Module
+        }
+    }
+}
---
+++ Powershell/PesterDemo/InstallPester/Install-Pester.ps1
@@ -0,0 +1,32 @@
+﻿function Install-Pester {
+    Param
+    (
+        $ModulePath = "C:\Users\johnm\Documents\WindowsPowerShell\Modules",
+        $PesterRepoUrl = "https://github.com/pester/Pester.git"
+    )
+    $PesterModulePath = "$ModulePath\Pester"
+    if ((Test-Path $PesterModulePath) -eq $true) {
+
+        Write-Host Deleting existing folder [$PesterModulePath] -ForegroundColor Gray
+        Remove-Item $PesterModulePath -Recurse -Force  
+    }
+
+    $current = Get-Location
+    Set-Location $ModulePath
+
+    Write-Host Cloneing https://github.com/pester/Pester.git to [$ModulePath]... -ForegroundColor gray
+    $exp = '& "C:\Program Files\Git\bin\git.exe" clone "https://github.com/pester/Pester.git"'
+    Invoke-Expression -Command $exp
+    Write-Host PLEASE NOTE: git reports standard output to stderr??? -ForegroundColor Green
+
+    Set-Location $current
+
+    Write-Host Installing Module... -ForegroundColor Gray
+    Import-Module -Name Pester
+    Write-Host Module Installed! -ForegroundColor Gray
+    
+    Write-Host Installed Commands: -ForegroundColor Green
+    Get-Module -Name Pester | Select -ExpandProperty ExportedCommands
+
+    Write-Host Setup Pester complete! -ForegroundColor Green
+}
