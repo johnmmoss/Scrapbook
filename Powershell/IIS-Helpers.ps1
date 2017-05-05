4e5d0c2f226626d488c7fca8e0689f665d9bcd15
johnmmoss <johnmmoss@gmail.com>
2017-05-05 16:32:55 +0100

    Update and rename Create-IIS-Website.ps1 to IIS-Helpers.ps1

---
+++ Powershell/IIS-Helpers.ps1
@@ -0,0 +1,16 @@
+﻿
+Import-Module WebAdministration
+
+
+function Build-Sln() {
+
+    # Build the solution, is this the right compiler version?
+    invoke-expression("C:\Windows\Microsoft.Net\Framework\v4.0.30319\msbuild.exe D:\svn\WebSite\src\Web.sln /v:q")
+}
+
+function Remove-WebSite() {
+
+    remove-item IIS:\AppPools\WebSite1 -Recurse
+    remove-item IIS:\Sites\Website1 -Recurse
+
+}
