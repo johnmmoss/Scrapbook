de51a529bd8adc0471d232b2e78f45aa2c106634
johnmmoss <johnmmoss@gmail.com>
2017-05-12 08:29:41 +0100

    Updated for windows client
    Added cmd helpers and shell helper modules

--- Powershell/Microsoft.Powershell_profile.ps1
+++ Powershell/Microsoft.Powershell_profile.ps1
@@ -5,45 +5,15 @@
 # Show Profiles: 
 #  -> $PROFILE | Format-List *
 
+# This was already here!
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\src\posh-git.psd1'
 
-function DEBUG {
-    Param(
-        $Message = $null
-    )
-
-    if($Message -ne $null) {
-
-        write-host $Message -ForegroundColor Gray
-    }
-}
-
-
-function INFO {
-    Param(
-        $Message = $null
-    )
-
-    if($Message -ne $null) {
-
-        write-host $Message -ForegroundColor Green
-    }
-}
-
-
-function WARN {
-    Param(
-        $Message = $null
-    )
-
-    if($Message -ne $null) {
-
-        write-host $Message -ForegroundColor Red
-    }
-}
-
-function show-profiles () {
+function Show-ProfilesLocation () {
     $PROFILE | Format-List * -Force
+}
+
+function Show-ModulesLocation () {
+	$env:PSModulePath.Split(';') 
 }
 
 INFO "Loading profile..." 
@@ -51,7 +21,16 @@ INFO "Setting prompt..."
 
 function prompt { "[" + $Env:username +  "] -> " }
 
+. E:\Code\Scrapbook\Powershell\Cmds-ServiceHelpers.ps1
+
+. E:\Code\Scrapbook\Powershell\Cmds-WebsiteHelpers.ps1
+
+# For windows shell: https://github.com/lzybkr/PSReadLine
+# Install with PowershellGalleryGet
+import-module PSReadLine
+
+# http://powertab.codeplex.com/
+# Install locally
+import-module E:\temp\PowerTab\PowerTab.psm1
+
 CLEAR
-
-
-
