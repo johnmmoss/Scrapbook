04fc5e18fd8e5b5410ae7e6482ce8df24c1ba152
John Moss <john.moss@zenith.co.uk>
2017-07-14 11:04:00 +0100

    Powershell pester and winrm scripts

---
+++ Powershell/Pester-Examples.ps1
@@ -0,0 +1,72 @@
+﻿
+
+    #
+    # Pester stub test
+    #
+    
+    # Function in Calc-Numbers.ps1
+    function Calc-Numbers {
+
+    }
+
+    # Test in Calc-Numbers.test.ps1
+    $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+    . "$here\$sut"
+
+    Describe "Calc-Numbers" {
+        It "does something useful" {
+            $true | Should Be $false
+        }
+    }
+
+    #
+    #  Testing parameters
+    #
+    It "when parameter is null or empty" {
+        
+		(( Update-Stats ) 2>&1) -match "Please provide the param!" | should be $true
+	}	
+    
+    #
+    # Simple Mocks
+    #
+    Mock Remove-Item  { }
+	
+	It "cleans directory of files" {
+
+		Clean-Directory
+
+		Assert-MockCalled Remove-Item
+	}
+
+    #
+    # Parameter filter mocks
+    #
+    Mock Copy-Item { } -ParameterFilter { $Recurse -eq $true; $Force -eq $true }
+
+    It "passes recurse to Copy-Item" {
+
+		Copy-MyFiles
+
+		Assert-MockCalled Copy-Item
+	}
+
+    #
+    # Wrap framework calls in powershell methods to mock
+    #
+    Mock Download-File
+
+    It "when source is null or empty" {
+        
+		Encrypt-Config
+
+        Assert-MockCalled Download-File
+    }	
+    
+    # Custom function
+    function Download-File() {
+	    Param($sourceUrl, $destinationPath)
+
+	    ((New-Object System.Net.WebClient).DownloadFile($sourceUrl, $destinationPath))
+    }
---
+++ Powershell/Windows-Remoting.ps1
@@ -0,0 +1,46 @@
+﻿#
+# Test-WSman - Test the WinRm service
+#
+test-wsman 192.148.111.12
+
+$password = ConvertTo-SecureString -String "P@sSwOrd" -AsPlainText -Force
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList "MyDomain\MyUsername",  $password
+
+Test-Wsman 192.148.111.12 -Authentication Default -Credential $Credential -Port 5985   
+
+# Test using kerberos, cant use IP address, must be a name
+Test-wsman MyComputer-01 -Authentication Kerberos -Credential $Credential
+
+#
+# WinRm - Windows Remote Management Command Line Tool
+#
+winrm get winrm/config 
+
+# Enumerate Listeners
+winrm e winrm/config
+winrm e winrm/config/listener
+
+# Create a listeners
+winrm quickconfig
+winrm quickconfig -transport:https
+
+#delete listener
+winrm delete winrm/config/Listener?Address=*+Transport=HTTP
+
+#
+# Win rs can be used to run remote commands
+#
+winrs -?
+
+#
+# PsSession uses WinRm to start a session
+# 
+Enter-PsSession MyServer-01
+cd ..\Desktop
+ls # Displays whats on your MyServer-01 desktop
+Exit-PSSession
+
+#
+# Invoke-Command to run a remote command on a server
+#
+Invoke-Command -ComputerName MyServer-01 -Command { ipconfig } 
