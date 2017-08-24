a21b7e9f4bcf737e959aa06c14e5fdf1f0427735
johnmmoss <johnmmoss@gmail.com>
2017-08-24 17:03:42 +0100

    Update Machine-Setup.ps1

--- Chocolatey/Machine-Setup.ps1
+++ Chocolatey/Machine-Setup.ps1
@@ -1,4 +1,6 @@
 
+Set-ExecutionPolicy AllSigned; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install 7zip -y
 choco install notepadplusplus -y
 choco install winmerge -y
@@ -7,11 +9,13 @@ choco install GoogleChrome -y
 choco install vim -y
 choco install sysinternals -y
 choco install treesizefree -y
-
 choco install git -y
-choco install gitkraken -y
 
-choco install VisualStudio2013Premium -y
-choco install VisualStudio2015Professional -y
-choco install VisualStudio2017Professional -y #  Needs further install see jpg
-choco install VisualStudioCode -y
+choco install slack -y
+choco install docker-for-windows -y
+choco install visualstudiocode -y
+choco install visualstudio2017professional -y
+
+choco install conemu
+choco install powertab -y
+Install-Module PSReadline # Powershell V5 and above has powershellget installed
